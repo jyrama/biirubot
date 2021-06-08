28955597485c48909919ed928d707b8b504adb07
Jyri Genral <jyri.genral@protonmail.ch>
2021-06-08 23:42:03 +0300

    Minor changes

--- biirubot/tests/beergium.rs
+++ biirubot/tests/beergium.rs
@@ -39,7 +39,57 @@ fn beergium_evil_twin_available() {
     let doc = kuchiki::parse_html().one(source);
 
     let products = doc.select("li.ajax_block_product").unwrap();
-    let available = products.filter_map(|x| x.as_node().select_first("span.sold_out").err());
+    let available = products.filter_map(|x| match x.as_node().select_first("span.sold_out") {
+        Ok(_) => None,
+        _ => Some(x),
+    });
 
     assert_eq!(available.count(), 14);
+}
+
+#[test]
+fn beergium_evil_twin_fructus_danica() {
+    let source = std::fs::read_to_string("tests/beergium_evil_twin.html").unwrap();
+    let doc = kuchiki::parse_html().one(source);
+
+    let products = doc.select("li.ajax_block_product").unwrap();
+    let fructus_danica: Vec<_> = products
+        .filter_map(|x| {
+            match x.as_node().select_first("a[title*=\"fructus danica\" i]") {
+                Ok(_) => Some(x),
+                Err(_) => None,
+            }
+                
+        })
+        .collect();
+
+    assert_eq!(fructus_danica.len(), 1);
+    let fructus_danica = fructus_danica.first().unwrap();
+
+    let title = fructus_danica.as_node().select_first("a.product-name").unwrap().text_contents();
+
+    let anchor = fructus_danica
+        .as_node()
+        .select_first("a.product-name")
+        .unwrap();
+    let anchor_attrs = anchor.attributes.borrow();
+    let link = anchor_attrs.get("href").unwrap();
+
+    let manufacturer = fructus_danica
+        .as_node()
+        .select_first("p.pro_list_manufacturer")
+        .unwrap()
+        .text_contents();
+
+    let price = fructus_danica
+        .as_node()
+        .select_first("span[itemprop=\"price\"]")
+        .unwrap();
+    let price = price.attributes.borrow();
+    let price = price.get("content").unwrap();
+
+    assert_eq!(title, "Evil Twin NYC Fructus Danica 3 Mulberry, Raspberry, Calamansi CANS 47cl");
+    assert_eq!(link, "https://www.beergium.com/en/evil-twin-brewing/4570-evil-twin-nyc-fructus-danica-3-mulberry-raspberry-calamansi-cans-47cl.html");
+    assert_eq!(manufacturer, "Evil Twin");
+    assert_eq!(price, "13.99");
 }
